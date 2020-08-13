d8721f587dadf01bde2157d2d0895661eccbf979
Prachi <pracheehere@gmail.com>
2020-08-13 17:58:11 +0530

    first commit

---
+++ src/main/java/com/edu/HelloController.java
@@ -0,0 +1,13 @@
+package com.edu;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayhello(){
+		return "Hello world!!";
+	}
+}
---
+++ src/main/java/com/edu/SpringBootWebSslDemoApplication.java
@@ -0,0 +1,13 @@
+package com.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSslDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSslDemoApplication.class, args);
+	}
+
+}
